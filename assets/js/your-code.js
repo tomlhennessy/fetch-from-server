2cd671d1a0170e0aab6c2f959604406bd3233add
Tom Hennessy <tomlhennessy@outlook.com>
2024-10-25 14:58:05 +1000

    used fetch api to interact with server

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,48 @@
 export function getAllDogs() {
-    // Your code here
+    return fetch("/dogs");
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+    return fetch("/dogs/2");
 }
 
 export function postNewDog() {
-    // Your code here
+    const body = new URLSearchParams({
+        name: "Buddy", // hardcoded name
+        age: "3"       // hardcoded age
+    });
+
+    const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: body
+    };
+
+    return fetch("/dogs", options);
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+    const body = new URLSearchParams({
+        name: name,
+        age: age
+    })
+
+    const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded"},
+        body: body
+    }
+
+    return fetch("/dogs", options);
 }
 
 export function deleteDog(id) {
-      // Your code here
+      const options = {
+        method: "POST",
+        headers: {
+            "AUTH": "ckyut5wau0000jyv5bsrud90y"
+        }
+      }
+
+      return fetch(`/dogs/${id}/delete`, options);
 }
